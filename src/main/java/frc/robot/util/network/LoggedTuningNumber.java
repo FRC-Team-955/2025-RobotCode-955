ffce2415606fa061bd9c99638cf1336863eb6970
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-02-14 18:36:00 -0800

    Fix tuning number being created but not paying attention to updates if not in tuning mode

--- src/main/java/frc/robot/util/network/LoggedTuningNumber.java
+++ src/main/java/frc/robot/util/network/LoggedTuningNumber.java
@@ -5,30 +5,76 @@ import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 import java.util.HashSet;
 
-public class LoggedTuningNumber extends LoggedNetworkNumber {
-    private final HashSet<Integer> hashCodesGottenChange = new HashSet<>();
-    private double lastValue = get();
-
-    public LoggedTuningNumber(String key) {
-        super(key);
-    }
+public class LoggedTuningNumber {
+    private LoggedTuningNumberInner inner;
+    private final double defaultValue;
 
     public LoggedTuningNumber(String key, double defaultValue) {
-        super(key, defaultValue);
+        if (Constants.tuningMode) {
+            inner = new LoggedTuningNumberInner(key, defaultValue);
+        } else {
+            this.defaultValue = defaultValue;
+        }
     }
 
     public boolean hasChanged(int hashCode) {
-        if (Constants.tuningMode && !hashCodesGottenChange.contains(hashCode)) {
-            hashCodesGottenChange.add(hashCode);
-            return true;
+        if (Constants.tuningMode) {
+            return inner.hasChanged(hashCode);
         } else {
             return false;
         }
     }
 
-    @Override
-    public void periodic() {
+    // FIXME: If you need these methods, you should update hasChanged to work if not in tuning mode
+//    /** Updates the default value, which is used if no value in NT is found. */
+//    public void setDefault(double defaultValue) {
+//        if (Constants.tuningMode) {
+//            inner.setDefault(defaultValue);
+//        } else {
+//            this.valueIfNotTuningMode = defaultValue;
+//        }
+//    }
+//
+//    /**
+//     * Publishes a new value. Note that the value will not be returned by
+//     * {@link #get()} until the next cycle.
+//     */
+//    public void set(double value) {
+//        if (Constants.tuningMode) {
+//            inner.set(value);
+//        } else {
+//            valueIfNotTuningMode = value;
+//        }
+//    }
+
+    /** Returns the current value. */
+    public double get() {
         if (Constants.tuningMode) {
+            return inner.get();
+        } else {
+            return defaultValue;
+        }
+    }
+
+    private class LoggedTuningNumberInner extends LoggedNetworkNumber {
+        private final HashSet<Integer> hashCodesGottenChange = new HashSet<>();
+        private double lastValue = get();
+
+        private LoggedTuningNumberInner(String key, double defaultValue) {
+            super("/Tuning/" + removeSlash(key), defaultValue);
+        }
+
+        private boolean hasChanged(int hashCode) {
+            if (!hashCodesGottenChange.contains(hashCode)) {
+                hashCodesGottenChange.add(hashCode);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public void periodic() {
             super.periodic();
 
             double newValue = get();
