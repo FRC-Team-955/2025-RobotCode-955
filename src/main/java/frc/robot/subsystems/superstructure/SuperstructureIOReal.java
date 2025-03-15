9ff93e3346e97c840404e8b7724b03ec766d58e8
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-03-15 11:11:26 -0700

    Deploy auto-commit 2025-03-15 11:11:25

--- src/main/java/frc/robot/subsystems/superstructure/SuperstructureIOReal.java
+++ src/main/java/frc/robot/subsystems/superstructure/SuperstructureIOReal.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.superstructure;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class SuperstructureIOReal extends SuperstructureIO {
-    private final DigitalInput endEffectorBeamBreak = new DigitalInput(4);
+    private final DigitalInput endEffectorBeamBreak = new DigitalInput(2);
     private final DigitalInput funnelBeamBreak = new DigitalInput(8);
 
     public SuperstructureIOReal() {
