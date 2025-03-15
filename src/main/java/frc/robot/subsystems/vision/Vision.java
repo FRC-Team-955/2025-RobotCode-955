aabe035981fc0f221dd3611ee70055b892b9cf5d
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-03-15 10:28:02 -0700

    Deploy auto-commit 2025-03-15 10:28:01

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -124,7 +124,9 @@ public class Vision extends SubsystemBaseExt {
                                 || observation.pose().getX() < 0.0
                                 || observation.pose().getX() > aprilTagLayout.getFieldLength()
                                 || observation.pose().getY() < 0.0
-                                || observation.pose().getY() > aprilTagLayout.getFieldWidth();
+                                || observation.pose().getY() > aprilTagLayout.getFieldWidth()
+                                // Reject if station cam and reef cam connected
+                                || (metadata == AprilTagCamera.StationCam && aprilTagCameras.get(AprilTagCamera.ReefCam).inputs.connected);
 
                 // Add pose to log
                 robotPoses.add(observation.pose());
@@ -151,13 +153,11 @@ public class Vision extends SubsystemBaseExt {
                 angularStdDev *= metadata.stddevMultiplier;
 
                 // Send vision observation
-                if (metadata != AprilTagCamera.StationCam) {
-                    robotState.addVisionMeasurement(
-                            observation.pose().toPose2d(),
-                            observation.timestamp(),
-                            VecBuilder.fill(linearStdDev, linearStdDev, angularStdDev)
-                    );
-                }
+                robotState.addVisionMeasurement(
+                        observation.pose().toPose2d(),
+                        observation.timestamp(),
+                        VecBuilder.fill(linearStdDev, linearStdDev, angularStdDev)
+                );
             }
 
             // Log camera data
