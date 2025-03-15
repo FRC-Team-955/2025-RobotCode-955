03b437071328f17abc854744a64b109d5867ac01
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-03-15 12:59:06 -0700

    Revert "Deploy auto-commit 2025-03-15 12:53:35"
    This reverts commit b754facab37db3a45cd09537b1023e7c7ec586e8.

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -27,7 +27,7 @@ public class ElevatorConstants {
     public static final ElevatorLimit lowerLimit = new ElevatorLimit(0.25, -1.75);
 
     public static final double hardstopMeters = Units.inchesToMeters(20);
-    public static final double gentleMaxVelocityMetersPerSecond = 0.3;
+    public static final double gentleMaxVelocityMetersPerSecond = 0.4;
     /**
      * While we could calculate this based on the current velocity, it caused the gentle profile to be used
      * for only half of the loop cycles. This could probably be solved but I don't think it's worth the effort
