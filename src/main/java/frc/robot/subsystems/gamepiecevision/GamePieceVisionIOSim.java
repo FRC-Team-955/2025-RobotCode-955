ccdd50fbf312ee00e3eea8a5f3fcd0d87a0a56ef
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-03-24 08:17:36 -0700

    Delete weird debounce for leds not being on since that's not what would happen with a real robot

--- src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVisionIOSim.java
+++ src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVisionIOSim.java
@@ -1,12 +1,9 @@
 package frc.robot.subsystems.gamepiecevision;
 
-import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.superstructure.SuperstructureIOSim;
 
 public class GamePieceVisionIOSim extends GamePieceVisionIO {
     private boolean ledsOn = false;
-
-    private final Timer ledsNotOnTimer = new Timer();
 
     public GamePieceVisionIOSim() {
     }
@@ -15,17 +12,7 @@ public class GamePieceVisionIOSim extends GamePieceVisionIO {
     public void updateInputs(GamePieceVisionIOInputs inputs) {
         inputs.connected = true;
         inputs.ledsOn = ledsOn;
-        if (SuperstructureIOSim.gamePieceVisible && !ledsOn) {
-            if (!ledsNotOnTimer.isRunning()) {
-                ledsNotOnTimer.restart();
-            }
-            inputs.visible = !ledsNotOnTimer.hasElapsed(0.25);
-        } else {
-            if (ledsNotOnTimer.isRunning()) {
-                ledsNotOnTimer.stop();
-            }
-            inputs.visible = SuperstructureIOSim.gamePieceVisible;
-        }
+        inputs.visible = ledsOn && SuperstructureIOSim.gamePieceVisible;
     }
 
     @Override
