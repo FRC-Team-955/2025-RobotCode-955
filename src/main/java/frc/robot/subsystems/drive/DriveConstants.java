d2c519ecca7352b75356e9acfd2eb5040e988424
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-03-30 13:33:41 -0700

    Move to tuning

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -12,11 +12,11 @@ public class DriveConstants {
     public static final double assistMaximumDistanceMeters = Units.feetToMeters(5);
 
     public static final MoveToConfig moveToConfig = new MoveToConfig(
-            PIDF.ofPD(5, 0),
-            PIDF.ofPD(6, 0),
-            0.01,
+            PIDF.ofPD(4, 0.05),
+            PIDF.ofPD(4.5, 0.05),
+            0.02,
             0.05,
-            Units.degreesToRadians(1),
+            Units.degreesToRadians(2),
             Units.degreesToRadians(5)
     );
 
