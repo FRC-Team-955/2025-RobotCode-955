5892b7eaa9455b34e61e7dddd917be25abfd3422
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-04-01 16:44:16 -0700

    YAYYYY NEW CAMERA MOUNT THAT WORKS AMAZINGLY!!!!!!!!!!!!!!

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionConstants.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionConstants.java
@@ -61,10 +61,13 @@ public class AprilTagVisionConstants {
         ),
         ReefCam(
                 new Transform3d(
-                        Units.inchesToMeters(-7.5), Units.inchesToMeters(9.875), Units.inchesToMeters(24.75),
+                        // -7.75 x without elevator slant
+                        Units.inchesToMeters(-7.5), Units.inchesToMeters(10.25), Units.inchesToMeters(24.75),
                         // Rotation order matters
-                        new Rotation3d(0.0, Units.degreesToRadians(35), 0.0)
-                                .rotateBy(new Rotation3d(0.0, 0.0, Units.degreesToRadians(-155)))
+                        new Rotation3d(Units.degreesToRadians(20), 0.0, 0.0)
+                                // 35 pitch without elevator slant
+                                .rotateBy(new Rotation3d(0.0, Units.degreesToRadians(30), 0.0))
+                                .rotateBy(new Rotation3d(0.0, 0.0, Units.degreesToRadians(-153.5)))
                 ),
                 (cam) -> switch (Constants.identity) {
                     case COMPBOT -> new AprilTagVisionIOPhotonVision("ReefCam");
