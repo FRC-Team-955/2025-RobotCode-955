3580301677510280342140d56bee3dd35715cd0b
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-04-01 18:38:24 -0700

    Revert "Increase move to slightly"
    This reverts commit b5a5b8e9f9d86b02052336d3d1fe96b20872fa72.

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -12,7 +12,7 @@ public class DriveConstants {
     public static final double assistMaximumDistanceMeters = Units.feetToMeters(5);
 
     public static final MoveToConfig moveToConfig = new MoveToConfig(
-            PIDF.ofPD(5.0, 0.05),
+            PIDF.ofPD(4.5, 0.05),
             PIDF.ofPD(4.5, 0.05),
             0.02,
             0.1,
