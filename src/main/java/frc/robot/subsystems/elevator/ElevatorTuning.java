8b02dca9456e4397156d3adf362e968cdb149c56
kaiyamashita08 <114874647+kaiyamashita08@users.noreply.github.com>
2025-04-04 16:23:57 -0700

    Deploy auto-commit 2025-04-04 16:23:56

--- src/main/java/frc/robot/subsystems/elevator/ElevatorTuning.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorTuning.java
@@ -16,7 +16,7 @@ public class ElevatorTuning {
     public static final LoggedTunableNumber stowGoalSetpoint =
             new LoggedTunableNumber("Elevator/Goal/Stow", 0.02);
     public static final LoggedTunableNumber scoreL1GoalSetpoint =
-            new LoggedTunableNumber("Elevator/Goal/ScoreL1", 0.3);
+            new LoggedTunableNumber("Elevator/Goal/ScoreL1", 0.33);
     public static final LoggedTunableNumber scoreL2GoalSetpoint =
             new LoggedTunableNumber("Elevator/Goal/ScoreL2", 0.69);
     public static final LoggedTunableNumber scoreL3GoalSetpoint =
