8ac398e4f0d679356012b57ecb2bcb31dba7862f
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-07-25 17:52:05 -0700

    I guess I forgot AprilTagVision

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
@@ -19,14 +19,14 @@ import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import frc.robot.RobotState;
 import frc.robot.Util;
-import frc.robot.util.subsystem.CommandBasedSubsystem;
+import frc.robot.util.subsystem.Periodic;
 import org.littletonrobotics.junction.Logger;
 
 import java.util.*;
 
 import static frc.robot.subsystems.apriltagvision.AprilTagVisionConstants.*;
 
-public class AprilTagVision extends CommandBasedSubsystem {
+public class AprilTagVision implements Periodic {
     private final RobotState robotState = RobotState.get();
 
     private final EnumMap<Camera, CameraData> cameras = Util.createEnumMap(Camera.class, Camera.values(), (cam) -> new CameraData(
