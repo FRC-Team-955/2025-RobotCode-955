625b19dbe2d45e5e73e958ec81e5dd4408e3ea2a
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-07-30 11:27:41 -0700

    Revert force flip and slow drive speed

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -62,7 +62,7 @@ public class DriveConstants {
                 PIDF.ofPD(3.5, 0),
                 PIDF.ofPD(3, 0),
                 new ModuleLimits(
-                        1.5,
+                        4.58,
                         15,
                         20
                 )
