4326752a763a4caafbd2cef403d2f8e99780f38a
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-07-30 17:55:37 -0700

    Increase manual speed

--- src/main/java/frc/robot/subsystems/funnel/FunnelTuning.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelTuning.java
@@ -12,7 +12,7 @@ public class FunnelTuning {
     public static final LoggedTunableNumber intakeGoalSetpoint =
             new LoggedTunableNumber("Funnel/Belt/Goal/Intake", Units.rotationsPerMinuteToRadiansPerSecond(900));
     public static final LoggedTunableNumber intakeManualGoalSetpoint =
-            new LoggedTunableNumber("Funnel/Belt/Goal/IntakeManual", Units.rotationsPerMinuteToRadiansPerSecond(200));
+            new LoggedTunableNumber("Funnel/Belt/Goal/IntakeManual", Units.rotationsPerMinuteToRadiansPerSecond(40));
     public static final LoggedTunableNumber ejectGoalSetpoint =
             new LoggedTunableNumber("Funnel/Belt/Goal/Eject", Units.rotationsPerMinuteToRadiansPerSecond(1000));
 }
