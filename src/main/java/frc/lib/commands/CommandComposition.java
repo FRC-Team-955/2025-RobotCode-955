cbd88aba6cd3fb3f094647d6747bc1dd994e7657
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-10-14 18:09:16 -0700

    goodbye

--- src/main/java/frc/lib/commands/CommandComposition.java
+++
@@ -1,7 +0,0 @@
-package frc.lib.commands;
-
-import edu.wpi.first.wpilibj2.command.Command;
-
-public abstract class CommandComposition extends Command {
-    public abstract void addCommands(Command... commands);
-}
--- src/main/java/frc/lib/subsystem/Periodic.java
+++
@@ -1,9 +0,0 @@
-package frc.lib.subsystem;
-
-public interface Periodic {
-    default void periodicBeforeCommands() {
-    }
-
-    default void periodicAfterCommands() {
-    }
-}
