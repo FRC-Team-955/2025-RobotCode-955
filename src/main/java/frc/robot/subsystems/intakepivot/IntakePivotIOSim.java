381a2bc5df4ff29b5b762fe1201dc842c0140cf1
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-11-20 19:55:26 -0800

    Implement pivot current limit

--- src/main/java/frc/robot/subsystems/intakepivot/IntakePivotIOSim.java
+++ src/main/java/frc/robot/subsystems/intakepivot/IntakePivotIOSim.java
@@ -5,7 +5,6 @@ import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.lib.PIDF;
 import frc.lib.Util;
@@ -14,7 +13,7 @@ import frc.lib.motor.RequestType;
 
 public class IntakePivotIOSim extends MotorIO {
     private static final double voltageLimit = 8.0;
-    private static final double currentLimit = 40.0;
+    private static final double currentLimitAmps = 30.0;
     private final DCMotor motor;
 
     private final SingleJointedArmSim armSim;
@@ -50,19 +49,28 @@ public class IntakePivotIOSim extends MotorIO {
             appliedVolts = pid.calculate(armSim.getAngleRads()) + ff.calculate(armSim.getAngleRads(), 0);
             appliedVolts = MathUtil.clamp(appliedVolts, -voltageLimit, voltageLimit);
 
-            // TODO: https://github.com/Shenzhen-Robotics-Alliance/maple-sim/blob/27ef554d86a62d8dba1a361cc5eca8919d4be1f9/project/src/main/java/org/ironmaple/simulation/motorsims/SimulatedMotorController.java#L65-L83
-            double maxTorqueForCurrentLimit = motor.getTorque(currentLimit);
-            double maxVoltsForCurrentLimit = motor.getVoltage(
-                    Math.copySign(maxTorqueForCurrentLimit, appliedVolts),
-                    armSim.getVelocityRadPerSec()
-            );
-            // maxVoltsForCurrentLimit will be
-            if (appliedVolts > 0) {
-                appliedVolts = Math.min(appliedVolts, maxVoltsForCurrentLimit);
-            } else {
-                appliedVolts = Math.max(appliedVolts, maxVoltsForCurrentLimit);
+            // https://github.com/Shenzhen-Robotics-Alliance/maple-sim/blob/27ef554d86a62d8dba1a361cc5eca8919d4be1f9/project/src/main/java/org/ironmaple/simulation/motorsims/SimulatedMotorController.java#L63-L84
+            final double kCurrentThreshold = 1.2;
+
+            final double motorCurrentVelocityRadPerSec = armSim.getVelocityRadPerSec();
+            final double requestedOutputVoltageVolts = appliedVolts;
+            final double currentAtRequestedVoltageAmps = motor.getCurrent(motorCurrentVelocityRadPerSec, requestedOutputVoltageVolts);
+
+            // Resource for current limiting:
+            // https://file.tavsys.net/control/controls-engineering-in-frc.pdf (sec 12.1.3)
+            double limitedVoltage = requestedOutputVoltageVolts;
+            final boolean currentTooHigh =
+                    Math.abs(currentAtRequestedVoltageAmps) > (kCurrentThreshold * currentLimitAmps);
+            if (currentTooHigh) {
+                final double limitedCurrent = Math.copySign(currentLimitAmps, currentAtRequestedVoltageAmps);
+                limitedVoltage = motor.getVoltage(motor.getTorque(limitedCurrent), motorCurrentVelocityRadPerSec);
             }
-            appliedVolts = Math.copySign(maxVoltsForCurrentLimit, Timer.getTimestamp() % 1 - 0.5);
+
+            // ensure the current limit doesn't cause an increase to output voltage
+            if (Math.abs(limitedVoltage) > Math.abs(requestedOutputVoltageVolts))
+                limitedVoltage = requestedOutputVoltageVolts;
+
+            appliedVolts = limitedVoltage;
         }
 
         armSim.setInputVoltage(appliedVolts);
