8539e7d5ad6ca9c04f653e748ab6291da21bf1a3
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-12-06 13:18:59 -0800

    Add real camera constants

--- src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVisionConstants.java
+++ src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVisionConstants.java
@@ -7,6 +7,8 @@ import edu.wpi.first.math.util.Units;
 import frc.lib.math.TaitBryanAngles;
 import frc.robot.BuildConstants;
 
+import static frc.robot.subsystems.drive.DriveConstants.driveConfig;
+
 public class GamePieceVisionConstants {
     /** Minimum distance between two seen coral for them to be counted as the same coral */
     static final double minDistanceForSameCoralMeters = 1;
@@ -16,10 +18,16 @@ public class GamePieceVisionConstants {
 
     static final Camera camera = new Camera(
             new Transform3d(
-                    new Translation3d(0, 0, 0.5),
-                    new Rotation3d(Units.degreesToRadians(30.0), 0, 0)
-                            .rotateBy(new Rotation3d(0, Units.degreesToRadians(30.0), 0))
-                            .rotateBy(new Rotation3d(0, 0, Units.degreesToRadians(30.0)))
+                    new Translation3d(
+                            Units.inchesToMeters(8.452489),
+                            Units.inchesToMeters(9.467625),
+                            // Carpet to bottom of frame
+                            (driveConfig.wheelRadiusMeters() - Units.inchesToMeters(0.247775))
+                                    // Bottom of frame to camera
+                                    + Units.inchesToMeters(8.604877)
+                    ),
+                    new Rotation3d(0, Units.degreesToRadians(25.0), 0)
+                            .rotateBy(new Rotation3d(0, 0, Units.degreesToRadians(-20.0)))
             ),
             Units.degreesToRadians(62.5),
             Units.degreesToRadians(48.9)
