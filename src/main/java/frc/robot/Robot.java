50fa04c288cad96db42cc38d4d62833607a467ca
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-12-05 21:08:47 -0800

    Remove intake pivot subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -133,7 +133,6 @@ public class Robot extends LoggedRobot {
                 robotContainer.operatorDashboard,
 
                 // Subsystems - the order of these doesn't matter
-                robotContainer.intakePivot,
 
                 // Misc
                 robotContainer.canLogger
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,6 @@ package frc.robot;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -12,7 +11,6 @@ import frc.lib.CANLogger;
 import frc.lib.commands.CommandsExt;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.goals.WheelRadiusCharacterizationGoal;
-import frc.robot.subsystems.intakepivot.IntakePivot;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -34,7 +32,6 @@ public class RobotContainer {
     /* Subsystems */
     // Note: order does matter
     public final Drive drive = Drive.get();
-    public final IntakePivot intakePivot = IntakePivot.get();
 
     public RobotContainer() {
         addAutos();
